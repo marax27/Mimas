de492795177a7606758c11943ea5087ce8cf2c7a
marax27 <kacpert314@gmail.com>
2022-07-08 15:00:33 +0100

    Add dashboard with a hardcoded chart

---
+++ Mimas.Server/Mimas.Server.Web/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mimas.Server.Web.Pages
+{
+    public class ProgressChartViewModel
+    {
+        public int Dark { get; }
+        public int Danger { get; }
+        public int Warning { get; }
+        public int Success { get; }
+
+        public float DarkPercentage { get; private set; }
+        public float DangerPercentage { get; private set; }
+        public float WarningPercentage { get; private set; }
+        public float SuccessPercentage { get; private set; }
+
+        public ProgressChartViewModel(int dark, int danger, int warning, int success)
+        {
+            (Dark, Danger, Warning, Success) = (dark, danger, warning, success);
+
+            var sum = dark + danger + warning + success;
+            if (sum == 0) return;
+
+            DarkPercentage = dark * 1f / sum;
+            DangerPercentage = danger * 1f / sum;
+            WarningPercentage = warning * 1f / sum;
+            SuccessPercentage = success * 1f / sum;
+        }
+
+        public string GetDarkCssPercentage(float totalWidth) => $"{DarkPercentage * totalWidth}%";
+        public string GetDangerCssPercentage(float totalWidth) => $"{DangerPercentage * totalWidth}%";
+        public string GetWarningCssPercentage(float totalWidth) => $"{WarningPercentage * totalWidth}%";
+        public string GetSuccessCssPercentage(float totalWidth) => $"{SuccessPercentage * totalWidth}%";
+    }
+
+    public class DashboardModel : PageModel
+    {
+        private readonly ILogger<DashboardModel> _logger;
+
+        public ProgressChartViewModel? ItemsChart { get; private set; }
+
+        public DashboardModel(ILogger<DashboardModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ItemsChart = new(6, 12, 44, 20);
+        }
+    }
+}
