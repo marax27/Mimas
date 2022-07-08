faaa4503d190fe40687facdf5fd33e286bc1dd52
marax27 <kacpert314@gmail.com>
2022-07-09 00:29:39 +0200

    Update dashboard chart and box table

--- Mimas.Server/Mimas.Server.Web/Pages/Dashboard.cshtml.cs
+++ Mimas.Server/Mimas.Server.Web/Pages/Dashboard.cshtml.cs
@@ -1,53 +1,85 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mimas.Server.Application.Ports;
+using Mimas.Server.Web.ViewModels;
 
 namespace Mimas.Server.Web.Pages
 {
-    public class ProgressChartViewModel
-    {
-        public int Dark { get; }
-        public int Danger { get; }
-        public int Warning { get; }
-        public int Success { get; }
-
-        public float DarkPercentage { get; private set; }
-        public float DangerPercentage { get; private set; }
-        public float WarningPercentage { get; private set; }
-        public float SuccessPercentage { get; private set; }
-
-        public ProgressChartViewModel(int dark, int danger, int warning, int success)
-        {
-            (Dark, Danger, Warning, Success) = (dark, danger, warning, success);
-
-            var sum = dark + danger + warning + success;
-            if (sum == 0) return;
-
-            DarkPercentage = dark * 1f / sum;
-            DangerPercentage = danger * 1f / sum;
-            WarningPercentage = warning * 1f / sum;
-            SuccessPercentage = success * 1f / sum;
-        }
-
-        public string GetDarkCssPercentage(float totalWidth) => $"{DarkPercentage * totalWidth}%";
-        public string GetDangerCssPercentage(float totalWidth) => $"{DangerPercentage * totalWidth}%";
-        public string GetWarningCssPercentage(float totalWidth) => $"{WarningPercentage * totalWidth}%";
-        public string GetSuccessCssPercentage(float totalWidth) => $"{SuccessPercentage * totalWidth}%";
-    }
-
     public class DashboardModel : PageModel
     {
         private readonly ILogger<DashboardModel> _logger;
+        private readonly IItemRepository _itemRepository;
+        private readonly IBoxRepository _boxRepository;
+        private readonly ITimeProvider _timeProvider;
 
         public ProgressChartViewModel? ItemsChart { get; private set; }
 
-        public DashboardModel(ILogger<DashboardModel> logger)
+        public DashboardModel(ILogger<DashboardModel> logger,
+            IItemRepository itemRepository, IBoxRepository boxRepository, ITimeProvider timeProvider)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _itemRepository = itemRepository ?? throw new ArgumentNullException(nameof(itemRepository));
+            _boxRepository = boxRepository ?? throw new ArgumentNullException(nameof(boxRepository));
+            _timeProvider = timeProvider ?? throw new ArgumentNullException(nameof(timeProvider));
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-            ItemsChart = new(6, 12, 44, 20);
+            ItemsChart = await GenerateItemsChart();
+        }
+
+        private async Task<ProgressChartViewModel> GenerateItemsChart()
+        {
+            var (dark, danger, warning, success) = (0, 0, 0, 0);
+
+            var (boxTask, itemTask) = (_boxRepository.GetAll(), _itemRepository.GetAll());
+            await Task.WhenAll(boxTask, itemTask);
+            var (allBoxes, allItems) = ((await boxTask).ToArray(), await itemTask);
+            var timeNow = _timeProvider.GetTimeNow();
+
+            foreach (var item in allItems)
+            {
+                if (ItemValidation.FindErrors(item).Any())
+                {
+                    ++danger;
+                    continue;
+                }
+
+                if (item.AssignedBoxShortId is null)
+                {
+                    ++dark;
+                    continue;
+                }
+
+                var assignedBox = allBoxes.SingleOrDefault(box => box.ShortId.Equals(item.AssignedBoxShortId));
+                if (assignedBox is null)
+                {
+                    ++danger;
+                }
+                else
+                {
+                    var hasBeenDelivered = assignedBox.DeliveredOn != null && assignedBox.DeliveredOn <= timeNow;
+                    if (hasBeenDelivered)
+                        ++success;
+                    else
+                        ++warning;
+                }
+            }
+
+            return new(dark, danger, warning, success);
+        }
+    }
+
+    internal static class ItemValidation
+    {
+        public static IReadOnlyCollection<string> FindErrors(ItemViewModel item)
+        {
+            var result = new List<string>();
+
+            if (item.Count < 1) result.Add($"Invalid item count: {item.Count}");
+            if (string.IsNullOrWhiteSpace(item.Name)) result.Add("Empty item name.");
+
+            return result;
         }
     }
 }
---
+++ Mimas.Server/Mimas.Server.Web/ViewModels/ProgressChartViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Mimas.Server.Web.ViewModels;
+
+public class ProgressChartViewModel
+{
+    public int Dark { get; }
+    public int Danger { get; }
+    public int Warning { get; }
+    public int Success { get; }
+
+    public float DarkPercentage { get; private set; }
+    public float DangerPercentage { get; private set; }
+    public float WarningPercentage { get; private set; }
+    public float SuccessPercentage { get; private set; }
+
+    public ProgressChartViewModel(int dark, int danger, int warning, int success)
+    {
+        (Dark, Danger, Warning, Success) = (dark, danger, warning, success);
+
+        var sum = dark + danger + warning + success;
+        if (sum == 0) return;
+
+        DarkPercentage = dark * 1f / sum;
+        DangerPercentage = danger * 1f / sum;
+        WarningPercentage = warning * 1f / sum;
+        SuccessPercentage = success * 1f / sum;
+    }
+
+    public string GetDarkCssPercentage(float totalWidth) => $"{DarkPercentage * totalWidth}%";
+    public string GetDangerCssPercentage(float totalWidth) => $"{DangerPercentage * totalWidth}%";
+    public string GetWarningCssPercentage(float totalWidth) => $"{WarningPercentage * totalWidth}%";
+    public string GetSuccessCssPercentage(float totalWidth) => $"{SuccessPercentage * totalWidth}%";
+}
